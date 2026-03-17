d5148cd24859043e7e30332d2cbc46999304f925
陈奕波 <chenyibo@51cjml.com>
2026-03-17 14:14:11 +0800

    recommend subagent

--- subagents/recommend_project/src/tools/__init__.py
+++ subagents/recommend_project/src/tools/__init__.py
@@ -1,15 +1,13 @@
 """RecommendProject 工具集。"""
 
 from src.recommend_context import VehicleInfo
-from src.tools.query_car_key import query_car_key
-from src.tools.recommend_projects import (
-    recommend_projects,
+from src.tools.recommend_projects_api import (
+    recommend_projects_api,
     create_recommend_project_tool_map,
 )
 
 __all__ = [
     "VehicleInfo",
-    "query_car_key",
-    "recommend_projects",
+    "recommend_projects_api",
     "create_recommend_project_tool_map",
 ]
